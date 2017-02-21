c961946e373eb84a660e633d88769fc5a53e927f
trikitrok <trikitrok@gmail.com>
2017-02-21 22:33:40 +0100

    adding a contact

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,10 +1,11 @@
-(ns om-tut.core
+(ns ^:figwheel-always om-tut.core
   (:require-macros
     [cljs.core.async.macros :refer [go]])
   (:require
     [om.core :as om :include-macros true]
     [om.dom :as dom :include-macros true]
-    [cljs.core.async :refer [put! chan <!]]))
+    [cljs.core.async :refer [put! chan <!]]
+    [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -29,6 +30,23 @@
 
 (defn- display-name [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
+
+(defn- parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (== c 2) (assoc :middle middle)))))
+
+(defn- add-contact [data owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                        .-value
+                        parse-contact)]
+    (when new-contact
+      (om/transact! data :contacts #(conj % new-contact)))))
 
 (defn contact-view [contact owner]
   (reify
@@ -60,7 +78,11 @@
         (dom/h2 nil "Contact list")
         (apply dom/ul nil
           (om/build-all contact-view (:contacts data)
-            {:init-state {:delete delete}}))))))
+            {:init-state {:delete delete}}))
+        (dom/div nil
+          (dom/input #js {:type "text" :ref "new-contact"})
+          (dom/button #js {:onClick #(add-contact data owner)}
+            "Add contact"))))))
 
 (om/root
   contacts-view
